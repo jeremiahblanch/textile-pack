5d4b7984e576e6868faa46b1bd0d2fa8e6c7b9e7
Jeremiah Blanch <jeremiahblanch@gmail.com>
2024-09-13 10:23:25 +1000

    add pack one line tests

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import { TextilePack } from './lib/TextilePack.class';
 import { packItems } from './lib/adaptor';
 
 export { packItems, TextilePack };
+
+export * from './lib/types';
---
+++ tests/pack-one-line.test.ts
@@ -0,0 +1,71 @@
+import { packItems, TextilePack } from '../src';
+import { Item, PackingResult } from '../src';
+
+interface TestCase {
+  maxWidth: number;
+  blocks: [number, number][];
+}
+
+function verifyOneLine(result: PackingResult, items: Item[]): void {
+  // result.height should be equal to largest height in items
+  const largestHeight = items.reduce(
+    (highest, item) => (item.height > highest ? item.height : highest),
+    0,
+  );
+  expect(result.height).toBeCloseTo(largestHeight, 4);
+}
+
+describe('textile-bin-pack: Given 2 rectangles of equal height but differing width, where total width <= maxWidth, puts both side by side.', function () {
+  const testCases: TestCase[] = [
+    {
+      maxWidth: 350,
+      blocks: [
+        [195, 101],
+        [143, 102],
+      ],
+    },
+    {
+      maxWidth: 35,
+      blocks: [
+        [20, 10],
+        [15, 10],
+      ],
+    },
+    {
+      maxWidth: 4000,
+      blocks: [
+        [3000, 1000],
+        [1000, 1000],
+      ],
+    },
+    {
+      maxWidth: 35,
+      blocks: [
+        [19, 10],
+        [14, 10],
+      ],
+    },
+    {
+      maxWidth: 35,
+      blocks: [
+        [19.5, 10.1],
+        [14.3, 10.2],
+      ],
+    },
+  ];
+
+  testCases.forEach(({ maxWidth, blocks }) => {
+    const description =
+      blocks.map(([w, h]) => `${w}x${h}`).join(', ') + ` within ${maxWidth}`;
+
+    it(description, function () {
+      const bins: Item[] = blocks.map(([width, height]) => ({ width, height }));
+
+      const result = packItems(bins, { maxWidth });
+
+      expect(result.items).toBeDefined();
+      expect(result.items.length).toEqual(blocks.length);
+      verifyOneLine(result, result.items);
+    });
+  });
+});
